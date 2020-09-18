8d977bf9f5deebc5a199ddf064bb18d27ac61d7a
MhmdSairi <40028734+MhmdSairi@users.noreply.github.com>
2020-09-18 06:54:00 -0700

    Update Contact.vue

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -7,11 +7,11 @@
             <p>You can find me around the web:</p>
             <div class="columns is-multiline mt-3">
                 <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">Chat :</strong><a href="//t.me/mhmdsairi" class="has-text-white"> t.me/mhmdsairi</a></div>
-                <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">WhatsApp :</strong><a href="https://bit.ly/3caV1XX" class="has-text-white"> 085349945947</a></div>
-                <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">LinkedIn :</strong><a href="//linkedin.com/in/MhmdSairi" class="has-text-white"> linkkedin.com</a></div>
+                <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">WhatsApp :</strong><a href="https://bit.ly/3caV1XX" class="has-text-white"> +6285349945947</a></div>
+                <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">Blog :</strong><a href="https://ironimouse.blogspot.com/" class="has-text-white"> Ironimouse</a></div>
                 <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">GitHub :</strong><a href="//github.com/MhmdSairi" class="has-text-white"> github.com/MhmdSairi</a></div>
                 <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">GitLab :</strong><a href="//gitlab.com/MhmdSairi" class="has-text-white"> gitlab.com/MhmdSairi</a></div>
-                <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">Instagram :</strong><a href="//instagram.com/mhmdsairi_" class="has-text-white"> instagram.com</a></div>
+                <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">Instagram :</strong><a href="//instagram.com/mhmdsairi_" class="has-text-white"> mhmdsairi_</a></div>
             </div>
         </div>
     </section>
