cf23fd8d1203eba7481649a8f23d20d3229640b6
MhmdSairi <40028734+MhmdSairi@users.noreply.github.com>
2020-09-18 15:53:24 +0800

    Update Contact.vue

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -8,9 +8,9 @@
             <div class="columns is-multiline mt-3">
                 <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">Chat :</strong><a href="//t.me/mhmdsairi" class="has-text-white"> t.me/mhmdsairi</a></div>
                 <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">WhatsApp :</strong><a href="https://bit.ly/3caV1XX" class="has-text-white"> 085349945947</a></div>
-                <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">LinkedIn :</strong><a href="//linkedin.com/in/yanuaraditia" class="has-text-white"> linkedin.com/in/MhmdSairi</a></div>
-                <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">GitHub :</strong><a href="//github.com/yanuaraditia" class="has-text-white"> github.com/MhmdSairi</a></div>
-                <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">GitLab :</strong><a href="//gitlab.com/yanuaraditia" class="has-text-white"> gitlab.com/MhmdSairi</a></div>
+                <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">LinkedIn :</strong><a href="//linkedin.com/in/MhmdSairi" class="has-text-white"> linkkedin.com</a></div>
+                <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">GitHub :</strong><a href="//github.com/MhmdSairi" class="has-text-white"> github.com/MhmdSairi</a></div>
+                <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">GitLab :</strong><a href="//gitlab.com/MhmdSairi" class="has-text-white"> gitlab.com/MhmdSairi</a></div>
                 <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">Instagram :</strong><a href="//instagram.com/mhmdsairi_" class="has-text-white"> instagram.com</a></div>
             </div>
         </div>
