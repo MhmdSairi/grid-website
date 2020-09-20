4372e38052a4066ba7998d1b41c53fcb2f42bffa
MhmdSairi <40028734+MhmdSairi@users.noreply.github.com>
2020-09-20 16:47:02 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
     <footer class="section has-text-centered-widescreen">
         <div class="contaner">
             Copyright &copy; {{new Date().getFullYear() }} by <g-link to="/">MhmdSairi</g-link><br/>
-            Powered by <a href="//gridsome.org">Gridsome</a> & <a href="//mhmdsairi.vercel.app">MhmdSairi</a>
+            Powered by <a href="//ironimouse.blogspot.com">Ironimouse</a> & <a href="//mhmdsairi.vercel.app">MhmdSairi</a>
         </div>
     </footer>
 </template>
