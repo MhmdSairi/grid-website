16be6c42c656e313fca0ef0099d16689f3b15b3d
MhmdSairi© <40028734+MhmdSairi@users.noreply.github.com>
2020-12-11 05:51:06 -0800

    Update Contact.vue

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -6,12 +6,13 @@
             <h3 class="title is-5 has-text-primary">Stay In Touch</h3>
             <p>You can find me around the web:</p>
             <div class="columns is-multiline mt-3">
-                <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">Chat :</strong><a href="//t.me/mhmdsairi" class="has-text-white"> t.me/mhmdsairi</a></div>
+                <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">Telegram :</strong><a href="//t.me/mhmdsairi" class="has-text-white"> t.me/mhmdsairi</a></div>
                 <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">WhatsApp :</strong><a href="https://bit.ly/3caV1XX" class="has-text-white"> +6285349945947</a></div>
                 <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">Blog :</strong><a href="https://ironimouse.blogspot.com/" class="has-text-white"> Ironimouse</a></div>
                 <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">GitHub :</strong><a href="//github.com/MhmdSairi" class="has-text-white"> github.com/MhmdSairi</a></div>
                 <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">GitLab :</strong><a href="//gitlab.com/MhmdSairi" class="has-text-white"> gitlab.com/MhmdSairi</a></div>
                 <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">Instagram :</strong><a href="//instagram.com/mhmdsairi_" class="has-text-white"> mhmdsairi_</a></div>
+                <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">Instagram :</strong><a href="//mager.vercel.app/" class="has-text-white"> Mager</a></div>
 <div class="column is-6 is-4-widescreen"><strong class="has-text-primary">Sestyc :</strong><a href="https://bit.ly/31UjWLt" class="has-text-white"> MhmdSairi</a></div>
             </div>
         </div>
